f4c56a81cf65bbf232a08deb4c118f212494fe70
FrankLi123 <zoli@ucsd.edu>
2023-11-23 23:32:19 -0500

    Added new API handler functions for Frontend request #2

--- be/server/pages/views.py
+++ be/server/pages/views.py
@@ -37,28 +37,56 @@ def home_page_view(request):
 
 
 def generate_response_view(request):
-    arr = [
-        'Please take a break!',
-        'Don\'t forget to stretch for a while',
-        'You are awesome today!!!',
-        'Do not think about the things that you cannot change',
-        'There is a elder saying \"live in the moment\" '
-    ]
+    with connection.cursor() as cursor:
+        query = "SELECT content FROM words ORDER BY RAND() LIMIT 1"
+        cursor.execute(query)
+        random_word = cursor.fetchone()
 
-    if preference == None:
-        selected_sentence = random.choice(arr)
-        return JsonResponse({'sentence': selected_sentence})
-    
+    if random_word:
+        return JsonResponse({'sentence': random_word[0]})
     else:
-        openai.api_key= os.getenv("OPENAI_API_KEY")
-        prompt= f'can you give me a {preference} quote?'
+        return JsonResponse({'sentence': 'No words found in the database'})
 
+    # arr = [
+    #     'Please take a break!',
+    # ]
+
+    # if preference == None:
+    #     selected_sentence = random.choice(arr)
+    #     return JsonResponse({'sentence': selected_sentence})
+    
+    # else:
+    #     openai.api_key= os.getenv("OPENAI_API_KEY")
+    #     prompt= f'can you give me a {preference} quote?'
+
+    #     model = "text-davinci-003"
+    #     response = openai.Completion.create(engine=model, prompt=prompt, max_tokens =50)
+    #     generated_text =response.choices[0].text
+
+    #     print("answer is: " + generated_text)
+    #     return JsonResponse({'sentence': generated_text})
+
+
+
+def generate_ai_response_view(request):
+
+        param1 = request.GET.get('text')
+        print(f"received {param1}")
+
+        openai.api_key= os.getenv("OPENAI_API_KEY")
+
+        prompt= f'(please respond to a guy who is suffering from mental issue as an therapist within 70 token please): Here is the text:{param1}'
+        # prompt= '{param1}'
         model = "text-davinci-003"
-        response = openai.Completion.create(engine=model, prompt=prompt, max_tokens =50)
+        response = openai.Completion.create(engine=model, prompt=prompt, max_tokens =70)
         generated_text =response.choices[0].text
 
         print("answer is: " + generated_text)
         return JsonResponse({'sentence': generated_text})
+        # return JsonResponse({'sentence': "hey what's up man!"})
+
+
+
 
 
 # handler users setting inputs
@@ -81,8 +109,6 @@ def login_view(request):
     username = request.GET.get('username')
     password = request.GET.get('password')
 
-
-
     # print(f"received username: {param1}, password:{param2}")
     success, user_id, user_name = check_credentials(username,password)
 
@@ -102,7 +128,6 @@ def login_view(request):
     response = JsonResponse(response_data)
     response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
     return response
-    
 
 
 
@@ -111,11 +136,8 @@ def register_view(request):
     username = request.GET.get('username')
     password = request.GET.get('password')
     email = request.GET.get('email')
-
     print("register_view:received" +  username + "  " + password + "  " + email)
-
     with connection.cursor() as cursor:
-
         cursor.execute("INSERT INTO registration (username, password, email) VALUES (%s, %s, %s)", (username, password,email))
 
     response = JsonResponse({'success': True})
@@ -124,7 +146,6 @@ def register_view(request):
 
 # handler for user data collected from the use
 def collected_data_view(request):
-    print("debugg!!!")
     yesCount = request.GET.get('yesCount')
     noCount = request.GET.get('noCount')
     timeSpent = request.GET.get('timeSpent')
@@ -134,7 +155,6 @@ def collected_data_view(request):
     print("register_view:received yesCount: " +  yesCount + "  , noCount: " + noCount + "  ,timeSpent: " + timeSpent + " ,userId: " + userId + " , date is : " + dateStr)
 
     with connection.cursor() as cursor:
-
         #Check if there is an existing row for the user on this date
         cursor.execute("SELECT * FROM distractionAnswer WHERE user_id = %s AND date = %s", (userId, date))
 
@@ -147,10 +167,60 @@ def collected_data_view(request):
     return response
 
 
-def retrieve_data_view(request):
-
+def user_initial_setting_view(request):
     userId = request.GET.get('userId')
+    reasons = request.GET.get('reasons')
+    tips = request.GET.get('tips')
+    personality = request.GET.get('personality')
 
+    tableName = "user"
+    with connection.cursor() as cursor:
+        # Fetch the user instance from the database using the user_id
+        cursor.execute("SELECT * FROM user WHERE id = %s", [userId])
+        user_data = cursor.fetchone()
+        if user_data:
+            # Update the existing user's data
+            update_query = f"UPDATE {tableName} SET reasons = %s, tips = %s, personality = %s WHERE id = %s"
+            cursor.execute(update_query, [reasons, tips, personality, userId])
+            # Return a JSON response indicating success
+        else:
+            # Insert a new user with the provided data
+            insert_query = f"INSERT INTO {tableName} (id, reasons, tips, personality) VALUES (%s, %s, %s, %s)"
+            cursor.execute(insert_query, [userId, reasons, tips, personality])
+            
+    return JsonResponse({'message': 'User updated successfully'})
+            
+
+
+def retreive_reasons_view(request):
+
+    with connection.cursor() as cursor:
+            # Retrieve all reasons and tips from your table
+            cursor.execute("SELECT reason, tips FROM reasons")
+            data = cursor.fetchall()
+
+            # Extract reasons and tips from the result
+            reasons = [row[0] for row in data]
+            tips = [row[1] for row in data]
+
+            # For demonstration purposes, print the retrieved data
+            
+            for reason, tip in zip(reasons, tips):
+                print(f"Reason: {reason}, Tip: {tip}")
+            
+            # Prepare the response data
+            response_data = {
+                'reasons': reasons,
+                'tips': tips,
+            }
+
+    return JsonResponse(response_data)
+
+
+
+
+def retrieve_data_view(request):
+    userId = request.GET.get('userId')
     with connection.cursor() as cursor:
 
         #Check if there is an existing row for the user on this date
@@ -168,5 +238,71 @@ def retrieve_data_view(request):
             'dates' : dates,
             'yesCounts': yes_counts
         }
-
         return JsonResponse(res_data)
+    
+
+
+def retreive_user_data_view(request):
+    user_id = request.GET.get('userId')
+
+    with connection.cursor() as cursor:
+        # Check if there is an existing row for the user
+        cursor.execute("SELECT reasons, tips FROM user WHERE id = %s", (user_id,))
+        row = cursor.fetchone()
+
+        if row:
+            reasons = row[0].split(',') if row[0] else []
+            tips = row[1].split(',') if row[1] else []
+            res_data = {'reasons': reasons, 'tips': tips}
+        else:
+            res_data = {'reasons': [], 'tips': []}
+
+    return JsonResponse(res_data)
+
+def store_generated_texts_view(request):
+    
+    type = request.GET.get('type')
+
+    if type == "texts":
+
+        text_path = './data/words.txt'
+        file_path = os.path.join(os.path.dirname(__file__), text_path)
+        with open(file_path, 'r') as file:
+        
+            lines = file.readlines()
+
+        category = lines[0].strip()
+        group = lines[1].strip()
+        generated_words = [word.strip() for word in lines[2:]]
+
+        with connection.cursor() as cursor:
+            for word in generated_words:
+                    insert_query = """
+                        INSERT INTO words ( content, category, `group`)
+                        VALUES (%s, %s, %s)
+                    """
+                    values = ( word, category, group)
+                    cursor.execute(insert_query, values)
+        
+        return HttpResponse({'result': "ok"})
+
+    else:
+        text_path = './data/reasons.txt'
+        file_path = os.path.join(os.path.dirname(__file__), text_path)
+
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+        reasons_and_tips = [line.strip() for line in lines if line.strip()]
+
+        with connection.cursor() as cursor:
+            for i in range(0, len(reasons_and_tips), 2):
+                reason = reasons_and_tips[i].replace(":", "")
+                tip = reasons_and_tips[i + 1].replace("Tip: ", "")
+                insert_query = """
+                    INSERT INTO reasons (reason, tips)
+                    VALUES (%s, %s)
+                """
+                values = (reason, tip)
+                cursor.execute(insert_query, values)
+        
+        return HttpResponse({'result': "ok"})
